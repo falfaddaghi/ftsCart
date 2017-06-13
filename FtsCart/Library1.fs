a35e54fbb3bf0b2db3b69221d93318b2e709606d
sudqi <sudqiomar@gmail.com>
2017-06-13 16:30:33 +0300

    add test cases for buyitems and addItem

--- FtsCart/Library1.fs
+++ FtsCart/Library1.fs
@@ -1,4 +1,24 @@
 ﻿namespace FtsCart
+type Id =int
+type Name=string
+type Price=int
+type Item=
+    {
+        id:Id
+        name:Name
+        price:Price
+    }
+type Cart=
+    {
+        items:Item list
+        id:Id
+    }
 
-type Class1() = 
-    member this.X = "F#"
+ module transition=
+    let addItem cart item =
+        let items=item::cart.items
+        {cart with items=items}
+    let buyItems cart=
+        {cart with items=[]}
+
+
