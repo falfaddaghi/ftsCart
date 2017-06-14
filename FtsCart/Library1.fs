79007cb9206ccfa21800e54936fa7dd5ca5e3baf
sudqi <sudqiomar@gmail.com>
2017-06-14 14:32:41 +0300

    add decremnt,increment,setShipping info modify buyItems

--- FtsCart/Library1.fs
+++ FtsCart/Library1.fs
@@ -34,20 +34,54 @@ type UserCart=
 |EmptyCart of EmptyCartData
 |ActiveCart of ActiveCartData
 |BoughtCart of BoughtCartData 
+type User=
+    {
+        id:Id
+        userCart:ActiveCartData
+        history:BoughtCartData list
+
+    }
  module transition=
-    let calculateTotalPriceActiveCart (cart:ActiveCartData) =
+    let calculateTotalPriceWithShipping  (shippingCost:float) (itemsCost:float) :float=
+        itemsCost+shippingCost
+    let calculatePriceOfItems (cart:ActiveCartData) =
         cart.items|>List.sumBy(fun item->float item.price* float item.quantity)
+
     let addItemToEmptyCart (c:EmptyCartData) item=
-        ActiveCart{id=c.id;items=[item]}
+        ActiveCart{id=c.id;items=[item];shippingInfo=None}
+
     let addItemToActiveCart (ac:ActiveCartData) item=
         ActiveCart{ac with items=item::ac.items}       
+
     let removItemFromActiveCart (ac:ActiveCartData) itemId=
         let newItems=ac.items|>List.filter(fun x->x.id=itemId)
         ActiveCart{ac with items=newItems}
+
     let BuyItemsOfActiveCart (ac:ActiveCartData) =
-        let items=ac.items
-        let totalPrice=calculateTotalPriceActiveCart ac
-        let price=calculateTotalPriceActiveCart ac
-        BoughtCart{id=ac.id;items=ac.items;totalPrice=price}
+        match ac.shippingInfo with
+        |None->ActiveCart ac
+        |Some s->
+            let items=ac.items
+            let totalPrice=
+                ac
+                |>calculatePriceOfItems
+                |>calculateTotalPriceWithShipping s.shippingCost
+            BoughtCart{id=ac.id;items=ac.items;totalPrice=totalPrice}
+
+    let changeShippingInfoToActiveCart (ac:ActiveCartData) (info:ShippingInfo)=
+        {ac with shippingInfo=Some info}
+    let changeItemQuantity q (item:Item )=
+        if q>0 then
+            {item with quantity=q} 
+        else
+            item
+    let IncreamentQuantity (item:Item)=
+        {item with quantity=item.quantity+1}
+    let DecreamentQuantity (item:Item)=
+        if item.quantity > 1 then
+            {item with quantity=item.quantity-1}
+        else 
+            item
+        
      
     
